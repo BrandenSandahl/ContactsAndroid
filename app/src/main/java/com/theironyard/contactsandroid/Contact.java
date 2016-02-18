5988dedc7ec1f8a0a1634d36f1d9db7b7b74ae8d
BrandenSandahl <branden157@gmail.com>
2016-02-17 20:02:43 -0500

    added in the add feature and the remove feature.

---
+++ app/src/main/java/com/theironyard/contactsandroid/Contact.java
@@ -0,0 +1,39 @@
+package com.theironyard.contactsandroid;
+
+/**
+ * Created by branden on 2/17/16.
+ */
+public class Contact {
+
+    String name, phone;
+
+
+    public Contact(String name, String phone) {
+        setName(name);
+        setPhone(phone);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s)", name, phone);
+    }
+
+}
--- app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
+++ app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
@@ -2,12 +2,63 @@ package com.theironyard.contactsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemLongClickListener {
+
+    ArrayAdapter<Contact> contacts;
+    EditText nameField, phoneField;
+    Button addButton;
+    ListView contactView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //link up my stuffs
+        nameField = (EditText) findViewById(R.id.nameField);
+        phoneField = (EditText) findViewById(R.id.phoneField);
+        addButton = (Button) findViewById(R.id.addButton);
+        contactView = (ListView) findViewById(R.id.contactView);
+
+        //link the ArrayAdapter with the ListView
+        contacts = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1);
+        contactView.setAdapter(contacts);
+
+
+        addButton.setOnClickListener(this);
+        contactView.setOnItemLongClickListener(this);  //man..i forgot about this! Also note that it's setting the LONG CLICK. looooong click.
+
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        contacts.add(new Contact(nameField.getText().toString(), phoneField.getText().toString()));  //have to toString() here!!
+        nameField.setText("");
+        phoneField.setText("");
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        Contact contact = contacts.getItem(position);
+        contacts.remove(contact);
+        System.out.println(contact);
+        System.out.println(position);
+        return true;
+    }
+
+
 }
